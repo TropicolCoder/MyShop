9babf0260f6db40d89fb03708afe081e81153aed
TropicolCoder <jacobsbusayo@gmail.com>
2021-03-12 20:07:14 +0100

    Integrating Payment Gateway

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import braintree
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +148,15 @@ EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 
 EMAIL_USE_TLS = True
+
+# Braintree settings
+BRAINTREE_MERCHANT_ID = str(os.getenv('BRAINTREE_MERCHANT_ID'))  # Merchant ID
+BRAINTREE_PUBLIC_KEY = str(os.getenv('BRAINTREE_PUBLIC_KEY'))  # Public Key
+BRAINTREE_PRIVATE_KEY = str(os.getenv('BRAINTREE_PRIVATE_KEY'))  # Private key
+
+BRAINTREE_CONF = braintree.Configuration(
+    braintree.Environment.Sandbox,
+    BRAINTREE_MERCHANT_ID,
+    BRAINTREE_PUBLIC_KEY,
+    BRAINTREE_PRIVATE_KEY
+)
